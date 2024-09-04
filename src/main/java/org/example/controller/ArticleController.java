91429317d8a6718be6b89d5129c288d15829de2d
Lin <sunburst0203@outlook.com>
2024-09-04 14:39:13 +0800

    feature:新增文章

--- src/main/java/org/example/controller/ArticleController.java
+++ src/main/java/org/example/controller/ArticleController.java
@@ -1,8 +1,11 @@
 package org.example.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.pojo.Article;
 import org.example.pojo.Result;
+import org.example.service.ArticleService;
 import org.example.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -10,16 +13,16 @@ import java.util.Map;
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
-    @GetMapping("/list")
-    public Result list() {
-//        try {
-//            Map<String, Object> claims = JwtUtil.parseToken(token);
-//            return Result.success("文章列表");
-//        } catch (Exception e) {
-//            response.setStatus(401);
-//            return Result.error("未登录");
-//        }
-        return Result.success("文章列表");
+
+    @Autowired
+    private ArticleService articleService;
+
+
+    @PostMapping
+    public Result add(@RequestBody Article article){
+        articleService.add(article);
+        return Result.success();
     }
+
 
 }
---
+++ src/main/java/org/example/service/ArticleService.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+import org.example.pojo.Article;
+
+public interface ArticleService {
+
+    //新增文章
+    void add(Article article);
+}
